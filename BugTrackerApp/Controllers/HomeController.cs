c5929039c76b04c70e676c8826b8ac8642e0fd5f
Edith Molda <edith.22.mary@gmail.com>
2021-01-28 18:15:12 -0600

    Working form and post of form data

---
+++ BugTrackerApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTrackerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                ViewData["BugTitle"] = collection[1];
+                ViewData["Severity"] = collection[2];
+                ViewData["ProgramBehaviour"] = collection[3];
+                ViewData["ReproduceBehaviour"] = collection[4];
+
+                return View("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
